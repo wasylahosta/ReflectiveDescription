2b40d64db6629bdec471fe8cafa3decf2c90fbcb
Vasyl Ahosta <vasyl.ahosta@gmail.com>
2021-04-25 22:50:30 +0300

    WIP: structs and classes

--- Sources/ReflectiveDescription/ReflectiveDescription.swift
+++ Sources/ReflectiveDescription/ReflectiveDescription.swift
@@ -1,6 +1,22 @@
+import Foundation
 
-public func reflectiveDescription<T>(of subject: T) -> String {
-    return "\(type(of: subject)) = \(subject)"
+public func reflectiveDescription<T>(of subject: T, level: Int = 0) -> String {
+    let mirror = Mirror(reflecting: subject)
+    let typeName = "\(mirror.subjectType)"
+    if mirror.children.isEmpty {
+        let value = "\(subject)"
+        if value.contains(typeName) && !typeName.contains("String") {
+            return typeName + " {\n}"
+        } else {
+            return typeName + " = " + value
+        }
+    } else {
+        let singleIndent = "  "
+        let indent = Array(repeating: singleIndent, count: level).joined()
+        return mirror.children.reduce("\(typeName) {") { result, child in
+            result + "\n\(indent)\(singleIndent)\(child.label!): \(reflectiveDescription(of: child.value, level: level + 1))"
+        } + "\n\(indent)}"
+    }
 }
 
 public func reflectiveDescription<T>(of subject: T?) -> String {
--- Tests/ReflectiveDescriptionTests/ReflectiveDescriptionTests.swift
+++ Tests/ReflectiveDescriptionTests/ReflectiveDescriptionTests.swift
@@ -27,7 +27,50 @@ final class ReflectiveDescriptionTests: XCTestCase {
         assert(descriptionOf: optionalInt, isEqualTo: "Int? = nil")
     }
     
-    func testWithArrayOfStrings() {
+    func testWithEmptyStruct() {
+        let aStruct = EmptyStruct()
+        assert(descriptionOf: aStruct, isEqualTo: """
+                                                  EmptyStruct {
+                                                  }
+                                                  """
+        )
+    }
+    
+    func testWithEmptyClass() {
+        let aClass = EmptyClass()
+        assert(descriptionOf: aClass, isEqualTo: """
+                                                  EmptyClass {
+                                                  }
+                                                  """
+        )
+    }
+    
+    func testWithStructWithSimpleFields() {
+        let aStruct = StructA(int: 1, string: "str")
+        assert(descriptionOf: aStruct, isEqualTo: """
+                                                  StructA {
+                                                    int: Int = 1
+                                                    string: String = str
+                                                  }
+                                                  """
+        )
+    }
+    
+    func testWithClassWithEmbeddedStructAndIntField() {
+        let aClass = ClassA(int: 1, struct: StructA(int: 2, string: "Str"))
+        assert(descriptionOf: aClass, isEqualTo: """
+                                                  ClassA {
+                                                    int: Int = 1
+                                                    struct: StructA {
+                                                      int: Int = 2
+                                                      string: String = Str
+                                                    }
+                                                  }
+                                                  """
+        )
+    }
+    
+    func _testWithArrayOfStrings() {
         let strings = ["Str 0", "Str 1", "Str 2"]
         assert(descriptionOf: strings, isEqualTo: "[String] = [Str 0; Str 1; Str 2]")
     }
@@ -40,5 +83,28 @@ final class ReflectiveDescriptionTests: XCTestCase {
     private func assert<T>(descriptionOf subject: T?, isEqualTo expectedDescription: String, file: StaticString = #filePath, line: UInt = #line) {
         let actualDescription = reflectiveDescription(of: subject)
         XCTAssertEqual(expectedDescription, actualDescription, file: file, line: line)
+    }
+}
+
+struct EmptyStruct {
+}
+
+class EmptyClass {
+}
+
+struct StructA {
+    
+    let int: Int
+    let string: String
+}
+
+class ClassA {
+    
+    let int: Int
+    let `struct`: StructA
+    
+    init(int: Int, struct: StructA) {
+        self.int = int
+        self.struct = `struct`
     }
 }
