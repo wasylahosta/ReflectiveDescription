aa14cacbd9f58c989d23b8bd04bc24e4934a7af5
Vasyl Ahosta <vasyl.ahosta@gmail.com>
2021-05-02 23:23:12 +0300

    WIP: change simple types description format. Add Array, extend Optional

--- Sources/ReflectiveDescription/ReflectiveDescription.swift
+++ Sources/ReflectiveDescription/ReflectiveDescription.swift
@@ -1,25 +1,39 @@
 import Foundation
 
-public func reflectiveDescription<T>(of subject: T, level: Int = 0) -> String {
+public func reflectiveDescription(of subject: Any) -> String {
+    return reflectiveDescription(of: subject, level: 0)
+}
+
+private func reflectiveDescription(of subject: Any, level: Int, optionals: Int = 0) -> String {
     let mirror = Mirror(reflecting: subject)
     let typeName = "\(mirror.subjectType)"
+    let printableTypeName = (0..<optionals).reduce(typeName) { (name, _) in name + "?" }
+    let optionalTypeNamePrefix = "Optional<"
+    if typeName.hasPrefix(optionalTypeNamePrefix) {
+        if let value = mirror.children.first?.value {
+            return reflectiveDescription(of: value, level: level, optionals: optionals + 1)
+        } else {
+            let wrappedType = typeName.dropFirst(optionalTypeNamePrefix.count).dropLast()
+            return "(\(wrappedType)?) nil"
+        }
+    }
     if mirror.children.isEmpty {
         let value = "\(subject)"
         if value.contains(typeName) && !typeName.contains("String") {
-            return typeName + " {\n}"
+            return printableTypeName + " {\n}"
         } else {
-            return typeName + " = " + value
+            return "(\(printableTypeName)) \(value)"
         }
     } else {
         let singleIndent = "  "
         let indent = Array(repeating: singleIndent, count: level).joined()
-        return mirror.children.reduce("\(typeName) {") { result, child in
-            result + "\n\(indent)\(singleIndent)\(child.label!): \(reflectiveDescription(of: child.value, level: level + 1))"
+        return mirror.children.reduce("\(printableTypeName) {") { result, child in
+            var line = "\n\(indent)\(singleIndent)"
+            if let label = child.label {
+                line += "\(label): "
+            }
+            line += "\(reflectiveDescription(of: child.value, level: level + 1))"                
+            return result + line
         } + "\n\(indent)}"
     }
-}
-
-public func reflectiveDescription<T>(of subject: T?) -> String {
-    let value = subject.flatMap { "\($0)" } ?? "nil"
-    return "\(T.self)? = \(value)"
 }
--- Tests/ReflectiveDescriptionTests/ReflectiveDescriptionTests.swift
+++ Tests/ReflectiveDescriptionTests/ReflectiveDescriptionTests.swift
@@ -5,26 +5,31 @@ final class ReflectiveDescriptionTests: XCTestCase {
     
     func testWithString() {
         let string = "String Value"
-        let expectedDescription = "String = \(string)"
+        let expectedDescription = "(String) \(string)"
         assert(descriptionOf: string, isEqualTo: expectedDescription)
     }
     
     func testWithInteger() {
-        assert(descriptionOf: 1, isEqualTo: "Int = 1")
+        assert(descriptionOf: 1, isEqualTo: "(Int) 1")
     }
     
     func testWithDouble() {
-        assert(descriptionOf: 1.2, isEqualTo: "Double = 1.2")
+        assert(descriptionOf: 1.2, isEqualTo: "(Double) 1.2")
     }
     
     func testWithOptionalIntWithSomeValue() {
         let optionalInt: Int? = 1
-        assert(descriptionOf: optionalInt, isEqualTo: "Int? = 1")
+        assert(descriptionOf: optionalInt as Any, isEqualTo: "(Int?) 1")
     }
     
     func testWithOptionalNilInt() {
         let optionalInt: Int? = nil
-        assert(descriptionOf: optionalInt, isEqualTo: "Int? = nil")
+        assert(descriptionOf: optionalInt as Any, isEqualTo: "(Int?) nil")
+    }
+    
+    func testWithDoubleOptionalIntWithSomeValue() {
+        let optionalInt: Int?? = 4
+        assert(descriptionOf: optionalInt as Any, isEqualTo: "(Int??) 4")
     }
     
     func testWithEmptyStruct() {
@@ -36,6 +41,21 @@ final class ReflectiveDescriptionTests: XCTestCase {
         )
     }
     
+    func testWithOptionalNilStruct() {
+        let aStruct: EmptyStruct? = nil
+        assert(descriptionOf: aStruct as Any, isEqualTo: "(EmptyStruct?) nil")
+    }
+    
+    func testWithOptionalEmptyStructWithSomeValue() {
+        let aStruct: EmptyStruct? = EmptyStruct()
+        assert(descriptionOf: aStruct as Any, isEqualTo: """
+                                                  EmptyStruct? {
+                                                  }
+                                                  """
+        )
+    }
+    
+    
     func testWithEmptyClass() {
         let aClass = EmptyClass()
         assert(descriptionOf: aClass, isEqualTo: """
@@ -46,41 +66,73 @@ final class ReflectiveDescriptionTests: XCTestCase {
     }
     
     func testWithStructWithSimpleFields() {
-        let aStruct = StructA(int: 1, string: "str")
+        let aStruct = StructA(intVar: 1, stringVar: "str")
         assert(descriptionOf: aStruct, isEqualTo: """
                                                   StructA {
-                                                    int: Int = 1
-                                                    string: String = str
+                                                    intVar: (Int) 1
+                                                    stringVar: (String) str
+                                                  }
+                                                  """
+        )
+    }
+    
+    func testWithOptionalStructWithSimpleFields() {
+        let aStruct: StructA? = StructA(intVar: 1, stringVar: "str")
+        assert(descriptionOf: aStruct as Any, isEqualTo: """
+                                                  StructA? {
+                                                    intVar: (Int) 1
+                                                    stringVar: (String) str
                                                   }
                                                   """
         )
     }
     
     func testWithClassWithEmbeddedStructAndIntField() {
-        let aClass = ClassA(int: 1, struct: StructA(int: 2, string: "Str"))
+        let aClass = ClassA(intVar: 1, structVar: StructA(intVar: 2, stringVar: "Str"))
         assert(descriptionOf: aClass, isEqualTo: """
                                                   ClassA {
-                                                    int: Int = 1
-                                                    struct: StructA {
-                                                      int: Int = 2
-                                                      string: String = Str
+                                                    intVar: (Int) 1
+                                                    structVar: StructA {
+                                                      intVar: (Int) 2
+                                                      stringVar: (String) Str
                                                     }
                                                   }
                                                   """
         )
     }
     
-    func _testWithArrayOfStrings() {
+    func testWithArrayOfStrings() {
         let strings = ["Str 0", "Str 1", "Str 2"]
-        assert(descriptionOf: strings, isEqualTo: "[String] = [Str 0; Str 1; Str 2]")
+        assert(descriptionOf: strings, isEqualTo: """
+                                                  Array<String> {
+                                                    (String) Str 0
+                                                    (String) Str 1
+                                                    (String) Str 2
+                                                  }
+                                                  """
+        )
     }
     
-    private func assert<T>(descriptionOf subject: T, isEqualTo expectedDescription: String, file: StaticString = #filePath, line: UInt = #line) {
-        let actualDescription = reflectiveDescription(of: subject)
-        XCTAssertEqual(expectedDescription, actualDescription, file: file, line: line)
+    func testOptionalArrayOfAny() {
+        let array: [Any]? = [1, "Str", 1.2]
+        assert(descriptionOf: array as Any, isEqualTo: """
+                                                       Array<Any>? {
+                                                         (Int) 1
+                                                         (String) Str
+                                                         (Double) 1.2
+                                                       }
+                                                       """
+        )
     }
     
-    private func assert<T>(descriptionOf subject: T?, isEqualTo expectedDescription: String, file: StaticString = #filePath, line: UInt = #line) {
+    // Test Cases
+    // - Class with superclass
+    // - Array of Optional
+    // - Dictionary
+    // - Set
+    // - Multiple Optional with nil
+    
+    private func assert(descriptionOf subject: Any, isEqualTo expectedDescription: String, file: StaticString = #filePath, line: UInt = #line) {
         let actualDescription = reflectiveDescription(of: subject)
         XCTAssertEqual(expectedDescription, actualDescription, file: file, line: line)
     }
@@ -94,17 +146,17 @@ class EmptyClass {
 
 struct StructA {
     
-    let int: Int
-    let string: String
+    let intVar: Int
+    let stringVar: String
 }
 
 class ClassA {
     
-    let int: Int
-    let `struct`: StructA
+    let intVar: Int
+    let structVar: StructA
     
-    init(int: Int, struct: StructA) {
-        self.int = int
-        self.struct = `struct`
+    init(intVar: Int, structVar: StructA) {
+        self.intVar = intVar
+        self.structVar = structVar
     }
 }
