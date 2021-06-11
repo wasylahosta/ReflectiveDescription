97b492e04515be3654510250d424bc67137bc307
Vasyl Ahosta <vasyl.ahosta@gmail.com>
2021-06-11 17:24:12 +0300

    Refactor, use displayType instead of string checks

--- Sources/ReflectiveDescription/ReflectiveDescription.swift
+++ Sources/ReflectiveDescription/ReflectiveDescription.swift
@@ -1,47 +1,22 @@
 import Foundation
 
 public func reflectiveDescription(of subject: Any) -> String {
-    return reflectiveDescription(of: subject, level: 0)
+    return reflectiveDescription(of: subject, level: 0, nil)
 }
 
 private let optionalTypeNamePrefix = "Optional<"
 private let singleIndent = "  "
 
-private func reflectiveDescription(of subject: Any, level: Int, optionals: Int = 0, isDictionaryElement: Bool = false) -> String {
+private func reflectiveDescription(of subject: Any, level: Int, optionals: Int = 0, _ parentMirror: Mirror?) -> String {
     let mirror = Mirror(reflecting: subject)
-    let typeName = "\(mirror.subjectType)"
-    let printableTypeName = (0..<optionals).reduce(typeName) { (name, _) in name + "?" }
-    if typeName.hasPrefix(optionalTypeNamePrefix) {
+    if mirror.isOptional {
         if let value = mirror.children.first?.value {
-            return reflectiveDescription(of: value, level: level, optionals: optionals + 1)
+            return reflectiveDescription(of: value, level: level, optionals: optionals + 1, mirror)
         } else {
-            return "(\(rewrap(typeName))) nil"
-        }
-    }
-    if mirror.children.isEmpty {
-        let value = "\(subject)"
-        if value.contains(typeName) && !typeName.contains("String") {
-            return printableTypeName + " {\n}"
-        } else {
-            return "(\(printableTypeName)) \(value)"
+            return "(\(rewrap(mirror.typeName))) nil"
         }
     } else {
-        let isDictionary = typeName.hasPrefix("Dictionary<")
-        if !isDictionaryElement {
-            let indent = Array(repeating: singleIndent, count: level).joined()
-            let header = "\(printableTypeName) {"
-            let footer = "\n\(indent)}"
-            var childrenDescription = ""
-            if let children = mirror.superclassMirror?.children, !children.isEmpty {
-                childrenDescription += reflectiveDescription(of: children, indent, level, isDictionary)
-            }
-            childrenDescription += reflectiveDescription(of: mirror.children, indent, level, isDictionary)
-            return header + childrenDescription + footer
-        } else {
-            return mirror.children.map {
-                "\(reflectiveDescription(of: $0.value, level: level, isDictionaryElement: isDictionary))"
-            }.joined(separator: " : ")
-        }
+        return reflectiveDescription(ofNonOptional: subject, mirror, level, optionals, parentMirror)
     }
 }
 
@@ -58,17 +33,63 @@ private func unwrap(_ typeName: String) -> String {
     return String(typeName.dropFirst(optionalTypeNamePrefix.count).dropLast())
 }
 
-private func reflectiveDescription(of children: Mirror.Children, _ indent: String, _ level: Int, _ isDictionary: Bool) -> String {
+private func reflectiveDescription(ofNonOptional subject: Any, _ mirror: Mirror, _ level: Int, _ optionals: Int = 0, _ parentMirror: Mirror?) -> String {
+    let printableTypeName = (0..<optionals).reduce(mirror.typeName) { (name, _) in name + "?" }
+    if mirror.isSimpleType {
+        return "(\(printableTypeName)) \(subject)"
+    } else {
+        if mirror.children.isEmpty {
+            return printableTypeName + " {\n}"
+        } else {
+            if let parentMirror = parentMirror, parentMirror.isDictionary {
+                return mirror.children.map {
+                    "\(reflectiveDescription(of: $0.value, level: level, mirror))"
+                }.joined(separator: " : ")
+            } else {
+                let indent = Array(repeating: singleIndent, count: level).joined()
+                let header = "\(printableTypeName) {"
+                let footer = "\n\(indent)}"
+                var childrenDescription = ""
+                if let children = mirror.superclassMirror?.children, !children.isEmpty {
+                    childrenDescription += reflectiveDescription(of: children, indent, level, mirror)
+                }
+                childrenDescription += reflectiveDescription(of: mirror.children, indent, level, mirror)
+                return header + childrenDescription + footer
+            }
+        }
+    }
+}
+
+private func reflectiveDescription(of children: Mirror.Children, _ indent: String, _ level: Int, _ parentMirror: Mirror?) -> String {
     var lines = children.map { child -> String in
         var line = "\n\(indent)\(singleIndent)"
         if let label = child.label {
             line += "\(label): "
         }
-        line += "\(reflectiveDescription(of: child.value, level: level + 1, isDictionaryElement: isDictionary))"
+        line += "\(reflectiveDescription(of: child.value, level: level + 1, parentMirror))"
         return line
     }
-    if isDictionary {
+    if let parentMirror = parentMirror, parentMirror.isDictionary {
         lines.sort()
     }
     return lines.joined()
+}
+
+extension Mirror {
+    
+    var typeName: String {
+        "\(subjectType)"
+    }
+    
+    var isOptional: Bool {
+        displayStyle == .optional
+    }
+    
+    var isSimpleType: Bool {
+        displayStyle == nil
+    }
+    
+    var isDictionary: Bool {
+        displayStyle == .dictionary
+    }
 }
